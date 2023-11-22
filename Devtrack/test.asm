025713e0879f07909b310695f8798153bc037092
Aurelien FERNANDEZ <aurelien.fernandez@algosup.com>
2023-11-22 20:15:59 +0100

    Code: Added comments to code

--- Devtrack/test.asm
+++ Devtrack/test.asm
@@ -3,8 +3,8 @@ org 100h
 ; -----------------------------------------------
 ; We declare some initialized data.
 section .data
-    yes db '1',13,10,'$'   ; DOS printable string must be terminated by a dollar sign.
-    no db '0',13,10,'$'
+    truem db '1',13,10,'$'   ; true and false message.
+    falsem db '0',13,10,'$'
     end db 'end$'
     
 
@@ -13,71 +13,68 @@ section .data
 section .text
 
 mov eax, 0b100100100   ; init byte
-mov bx,  0b00000000
-mov cl,9
+mov bx,  0b00000000 
+mov cl,9 ;counter
 
 jmp reverse
 
 ShiftCheck:
-shr ax, 1
-jc yesm
+shr ax, 1 ;check 
+jc True ;if carry is set, jump to True
 
-jmp nom
+jmp False ;else jump to False
 
 
-yesm:
+True:
     inc cl
-    ; Print the message to the screen
-    mov ah, 9       ; AH=9 means "print string" function
-    mov dx, yes   ; Load the offset address of 'hello' into DX
-    int 21h         ; Call the DOS interrupt 21h to execute the function
+    mov ah, 9       
+    mov dx, truem   ; print "1"
+    int 21h         
     
     cmp cl, 8
-    jb ShiftCheck
+    jb ShiftCheck ;if byte is not finished continue the loop, else jump to end
 
     jmp endm
 
-nom:
+False:
     inc cl
-; Print the message to the screen
-    mov ah, 9       ; AH=9 means "print string" function
-    mov dx, no   ; Load the offset address of 'hello' into DX
-    int 21h         ; Call the DOS interrupt 21h to execute the function
+    mov ah, 9       
+    mov dx, falsem   ; print "0"
+    int 21h       
 
     cmp cl, 8
-    jb ShiftCheck
+    jb ShiftCheck ;if byte is not finished continue the loop, else jump to end
     
     jmp endm
 
 endm:
-; Print the message to the screen
-    mov ah, 9       ; AH=9 means "print string" function
-    mov dx, end   ; Load the offset address of 'hello' into DX
-    int 21h         ; Call the DOS interrupt 21h to execute the function
+    mov ah, 9       
+    mov dx, end   ; print "end"
+    int 21h         
 
     ; Exit the program
-    mov ah, 4Ch     ; AH=4Ch means "exit" function
-    xor al, al      ; Set AL to 0 (return code)
-    int 21h         ; Call the DOS interrupt 21h to exit the program
+    mov ah, 4Ch     
+    xor al, al      
+    int 21h       
 
 
   
   
 
 reverse:
-mov cx,0b00000001
+mov cx,0b00000001 ; this is the result of future operations
 dec cl
 shr eax, 1
-jc reverseTrue
+jc reverseTrue ;if the carry is set, jump, else continue the loop
 
 cmp cl,0
-jg reverse
+jg reverse ;if the byte is not finished conitnue, else go to "ShiftCheck"
 
 jmp ShiftCheck
 reverseTrue:
-shl cx,cl
-add ax, cx
+shl cx,cl ; shift until the position of the bit
+add ax, cx; add the bit to the result
 
 cmp cl,0
-jg reverse
+jg reverse ;if the byte is not finished conitnue, else go to "ShiftCheck"
 
